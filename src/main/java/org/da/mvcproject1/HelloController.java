6a99c514318ea5528a6e9606eaf6e6810df691b4
DA_promos <d.abramovskyi@gmail.com>
2021-08-27 22:49:56 +0200

    Spring MVC App1

---
+++ src/main/java/org/da/mvcproject1/HelloController.java
@@ -0,0 +1,13 @@
+package org.da.mvcproject1;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello-world")
+    public String sayHello(){
+        return "hello_world";
+    }
+}
